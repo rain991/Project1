4be407758fa595c67b05cc56b213076a41fa6cbb
Ivan Savenko <ivansavenko@MacBook-Air-Ivan.local>
2023-09-17 18:12:38 +0200

    Add other symbols

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -21,6 +21,7 @@ class MainActivity : AppCompatActivity() {
         val eightButton = findViewById<Button>(R.id.eight_button)
         val nineButton = findViewById<Button>(R.id.nine_button)
 
+        val pointButton = findViewById<Button>(R.id.point_button)
         val plusButton = findViewById<Button>(R.id.plus_button)
         val minusButton = findViewById<Button>(R.id.minus_button)
         val multiplyButton = findViewById<Button>(R.id.multiply_button)
@@ -28,10 +29,63 @@ class MainActivity : AppCompatActivity() {
 
         val resultTextView = findViewById<TextView>(R.id.result_TextView)
         val numberStringBuilder = StringBuilder()
-        oneButton.setOnClickListener{
-        numberStringBuilder.append("1")
+        zeroButton.setOnClickListener{
+        numberStringBuilder.append(0)
             resultTextView.text = numberStringBuilder
         }
+        oneButton.setOnClickListener{
+            numberStringBuilder.append(1)
+            resultTextView.text = numberStringBuilder
+        }
+        twoButton.setOnClickListener{
+            numberStringBuilder.append(2)
+            resultTextView.text = numberStringBuilder
+        }
+        threeButton.setOnClickListener{
+            numberStringBuilder.append(3)
+            resultTextView.text = numberStringBuilder
+        }
+        fourButton.setOnClickListener{
+            numberStringBuilder.append(4)
+            resultTextView.text = numberStringBuilder
+        }
+        fiveButton.setOnClickListener{
+            numberStringBuilder.append(5)
+            resultTextView.text = numberStringBuilder
+        }
+        sixButton.setOnClickListener{
+            numberStringBuilder.append(6)
+            resultTextView.text = numberStringBuilder
+        }
+        sevenButton.setOnClickListener{
+            numberStringBuilder.append(7)
+            resultTextView.text = numberStringBuilder
+        }
+        eightButton.setOnClickListener{
+            numberStringBuilder.append(8)
+            resultTextView.text = numberStringBuilder
+        }
+        nineButton.setOnClickListener{
+            numberStringBuilder.append(9)
+            resultTextView.text = numberStringBuilder
+        }
+        plusButton.setOnClickListener{
+            numberStringBuilder.append("+")
+            resultTextView.text = numberStringBuilder
+        }
+        minusButton.setOnClickListener{
+            numberStringBuilder.append("-")
+            resultTextView.text = numberStringBuilder
+        }
+        multiplyButton.setOnClickListener{
+            numberStringBuilder.append("*")
+            resultTextView.text = numberStringBuilder
+        }
+        pointButton.setOnClickListener{
+            numberStringBuilder.append(".")
+            resultTextView.text = numberStringBuilder
+        }
+
     }
 
 }
